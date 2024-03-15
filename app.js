a1ad486b5c780890dea37904d34a5ffe761daf5d
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-15 13:47:09 +0100

    Classe criada com as funcionalidades básicas

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 class App {
     constructor() {
-        this.app = new express;
+        this.app = express();
+        this.middlewares();
+        this.routes();
     }
 
     middlewares(){
-
+        this.app.use(express.urlencoded({extended: true})); // Para receber params na url
+        this.app.use(express.json()); // habilita o uso de json
     }
 
     routes(){
 
     }
 }
+
+export default new App().app; // Exporta o objecto da classe App
