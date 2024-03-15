ee046f9e0af751a75ccc0da03bc885dfc9944f7f
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-15 15:40:27 +0100

    arq. de conexão com o banco de dados criado

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+// # -> Arq. de config da db
+import 'dotenv/config'; // Importa o dotenv
+module.exports = {
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    timezone:'Africa/Luanda',
+    define: {
+        timestamps: true // Define o registo automático de data de criação e alteração de items do banco
+    },
+    dialectOptions: {
+        timezone:'Africa/Luanda',
+    }
+}
