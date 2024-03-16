051d0bdfe36ce83f329a354aaec39767f5165e93
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 03:50:07 +0100

    create-user-types-table migration criada e aplicada

---
+++ src/database/migrations/20240316022740-user-types-table-create.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user_types', {
+      user_types_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: { type: Sequelize.STRING(20), allowNull: false },
+      desc: { type: Sequelize.STRING(30), allowNull: false },
+      home: { type: Sequelize.STRING, allowNull: false }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_types');
+  }
+};
