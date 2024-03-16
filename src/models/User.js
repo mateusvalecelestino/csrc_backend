12f4b0058f55f826141a2414c1e0998139bde864
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 20:41:01 +0100

    Conexão para o model User adicionada

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model, DataTypes } from "sequelize";
+
+export default class User extends Model{
+    // static associate(models) {
+    //     this.belongsTo(models.UserType, { foreignKey: 'user_type', as: 'type' });
+    //     this.hasMany(models.User, { foreignKey: 'created_by', as: 'createdUsers' });
+    //     this.hasMany(models.User, { foreignKey: 'updated_by', as: 'updatedUsers' });
+    // }
+
+    static init(sequelize){
+        // Chama o método init da classe pai para definir os campos do modelo
+        super.init({
+            // Definição dos campos do model
+            users_id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+            name: DataTypes.STRING(30),
+            email: DataTypes.STRING,
+            password_hash: DataTypes.STRING,
+            // password: DataTypes.VIRTUAL, // Não existe no banco
+            user_type: DataTypes.INTEGER,
+            active: { type: DataTypes.TINYINT, defaultValue: 1 },
+            created_by: DataTypes.INTEGER,
+            updated_by: DataTypes.INTEGER
+        }, {
+            sequelize,
+        });
+        return this;
+    }
+}
