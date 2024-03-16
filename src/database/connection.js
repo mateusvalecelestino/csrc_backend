acf1f3c7a7ed2e9310855ccd868341143b8ec7f6
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 22:55:29 +0100

    BUG das associações resolvido

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,14 +8,18 @@ import User from "../models/User";
 const connection = new Sequelize(dbConfig); // Instância a conn. com a db
 
 // Guarda todos os models da bd
-const models = [ UserType, User ];
+const models = {
+    UserType, User
+};
 
-// Insere a conexão com o banco nos models
+// Insere a conexão nos models
 Object.keys(models).forEach(model => {
     models[model].init(connection)
 })
 
-// Verifica se os models possuem o método associate, se tiver executa passando o array de models
+// Verifica se um model possui associação e cria, caso possua
 Object.keys(models).forEach(model => {
-    if(model.associate) model.associate(models);
+    if (models[model].associate) {
+        models[model].associate(models)
+    }
 })
