f1af728dc05bfa927d77c05fed2585ee76978a4a
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 12:29:34 +0100

    Conexão adicionada ao model Users

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,8 +3,9 @@ import dbConfig from "../config/database";
 
 // Importa todos os models da bd
 import UserType from "../models/UserType";
+import User from "../models/User";
 
 // Guarda todos os models da bd
-const models = [UserType];
+const models = [UserType, User];
 const connection = new Sequelize(dbConfig); // Instância a conn. com a db
 models.forEach(model => model.init(connection)); // insere a conn. em todos os models
