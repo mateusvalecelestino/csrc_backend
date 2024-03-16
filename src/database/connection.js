d340fe73fa3a5b241ebfa7e443c1d912306cf397
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 22:26:10 +0100

    Config para associação criada

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,17 @@ import dbConfig from "../config/database";
 import UserType from "../models/UserType";
 import User from "../models/User";
 
-// Guarda todos os models da bd
-const models = [UserType, User];
 const connection = new Sequelize(dbConfig); // Instância a conn. com a db
-models.forEach(model => model.init(connection)); // insere a conn. em todos os models
+
+// Guarda todos os models da bd
+const models = [ UserType, User ];
+
+// Insere a conexão com o banco nos models
+Object.keys(models).forEach(model => {
+    models[model].init(connection)
+})
+
+// Verifica se os models possuem o método associate, se tiver executa passando o array de models
+Object.keys(models).forEach(model => {
+    if(model.associate) model.associate(models);
+})
