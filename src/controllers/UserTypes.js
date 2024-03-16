783edd359d808f11fc2ec354688ce89972a5dc91
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-16 05:25:50 +0100

    Rota get funcionando

--- src/controllers/UserTypes.js
+++ src/controllers/UserTypes.js
@@ -1,9 +1,14 @@
+import httpStatusCode from "../utils/HttpStatusCode";
+import UserType from "../models/UserType";
 class UserTypes{
-    get(req, res){
-        res.status(200).json({
-        status: true,
-        message: "api running | getting all user-types"
-    })
+    async get(req, res){
+        try {
+            // Carrega todos os tipos de usuários, apenas os campos passados como attributes
+            const userTypes = await  UserType.findAll({ attributes: ['user_types_id', 'name', 'desc'] });
+            res.json(userTypes);
+        }catch (e) {
+            res.status(httpStatusCode.HTTP_STATUS_SERVER_ERROR).json({success: false, message: "Erro ao carregar tipos de usuários. Tente mais tarde!"})
+        }
     }
 }
 export default new UserTypes; // exporta o objecto da classe
