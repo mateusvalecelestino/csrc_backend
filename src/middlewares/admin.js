2513a5c6779cf8a7f3497b9f6847c1a0fe71bb34
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 20:12:44 +0100

    Adicionada possiblidade do usuário alterar sua própria conta e correção do middleware admin

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -2,6 +2,6 @@ import httpStatusCode from "../utils/HttpStatusCode";
 import UserTypes from "../utils/UserTypes";
 // Middleware para verificar se o user é admin
 export default (req, res, next) => {
-    if (req.userId !== UserTypes.ADMIN) return res.status(httpStatusCode.UNAUTHORIZED).json({ message: "Acesso não autorizado!" });
+    if (req.userType !== UserTypes.ADMIN) return res.status(httpStatusCode.UNAUTHORIZED).json({ message: "Acesso não autorizado!" });
     return next();
 }
