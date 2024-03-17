e86af25a979875594e86a9bda821b82bc59196eb
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 07:45:59 +0100

    Validação dos dados de usuários criada

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model, DataTypes } from "sequelize";
+import bcrypt from 'bcryptjs';
 
 export default class User extends Model{
     // Definição as associações (joins)
@@ -10,19 +11,82 @@ export default class User extends Model{
 
     static init(sequelize){
         // Chama o método init da classe pai para definir os campos do modelo
+        // noinspection SpellCheckingInspection
         super.init({
             // Definição dos campos do model
-            name: DataTypes.STRING(30),
-            email: DataTypes.STRING,
-            password_hash: DataTypes.STRING,
-            // password: DataTypes.VIRTUAL, // Não existe no banco
-            user_type: DataTypes.INTEGER,
+            name: {
+                type: DataTypes.STRING(30),
+                defaultValue: "", // Valor default caso nada for enviado
+                // Validações dos dados (tudo que é validate)
+                validate: {
+                    len: {
+                        args: [3, 30],
+                        msg: "name de utilizador deve ter entre 3 e 30 caracteres."
+                    },
+                    is: {
+                        args: /^[A-Za-zÀ-ú\s]+$/,
+                        msg: "name deve possuir apenas caracteres alfabéticos."
+                    },
+                }
+            },
+            email: {
+                type: DataTypes.STRING,
+                defaultValue: "",
+                validate: {
+                    len: {
+                        args: [3, 255],
+                        msg: "email deve ter entre 3 e 255 caracteres."
+                    },
+                    isEmail: { msg: "email inválido." }
+                }
+            },
+            password_hash: {
+                type: DataTypes.STRING,
+                defaultValue: ""
+            },
+            password: {
+                type: DataTypes.VIRTUAL, // Não existe no banco
+                defaultValue: "",
+                validate: {
+                    len: {
+                        args: [8, 255],
+                        msg: "password deve ter entre 8 e 255 caracteres."
+                    },
+                    is: {
+                        args: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,64})/g,
+                        msg: "password deve possuir no mínimo uma letra minúscula, uma maiúscula, um número e um caracter especial."
+                    },
+                }
+            },
+            user_type: {
+                type: DataTypes.INTEGER,
+                defaultValue: "",
+                validate: {
+                    isInt: { msg: "user_type inválido." }
+                }
+            },
             active: { type: DataTypes.TINYINT, defaultValue: 1 },
-            created_by: DataTypes.INTEGER,
-            updated_by: DataTypes.INTEGER
+            created_by: {
+                type: DataTypes.INTEGER,
+                defaultValue: "",
+                validate: {
+                    isInt: { msg: "created_by inválido." }
+                }
+            },
+            updated_by: {
+                type: DataTypes.INTEGER,
+                defaultValue: "",
+                validate: {
+                    isInt: { msg: "updated_by inválido." }
+                }
+            },
         }, {
             sequelize,
         });
         return this;
     }
 }
+/*
+* O atributo defaultValue em todos os campos permitem lançar os erros quando nada for enviado
+* Dessa forma uma mensagem clara é enviada ao invés de: field doesn't have a defaultValue
+*/
