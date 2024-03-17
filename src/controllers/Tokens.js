a5b2a5f5c52f43f52867878ec0aa18f17bc19355
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 12:03:21 +0100

    controller para gestão de token criado

--- src/controllers/Tokens.js
+++ src/controllers/Tokens.js
@@ -1,11 +1,38 @@
 import httpStatusCode from "../utils/HttpStatusCode";
 import User from "../models/User";
+import isEmail from 'validator/lib/isEmail';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config' // Importa o dotenv
 class Tokens{
     async create(req, res){
         try {
-            res.json({success: true, message: "Token route working"});
+
+            const { password = "", email = ""} = req.body;
+            const regExPassword = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,255})/g;
+            const authErrorMessage = { success: false, message: "email e/ou senha são inválidos!" };
+
+            if(!email || !password) return res.status(httpStatusCode.BAD_REQUEST).json({ success: false, message: "email e senha são obrigatórios!" });
+
+            // Verifica se é um email válido ou se a senha é forte
+            if(!isEmail(email) || !regExPassword.test(password)) return res.status(httpStatusCode.BAD_REQUEST).json(authErrorMessage)
+
+            const user = await User.findOne({
+                where: { active: 1, email },
+                attributes: ['id', 'name', 'password_hash', 'user_type', ]
+            });
+            if(!user) return res.status(httpStatusCode.BAD_REQUEST).json(authErrorMessage);
+            if(!(await user.isValidPassword(password))) return res.status(httpStatusCode.BAD_REQUEST).json(authErrorMessage);
+
+            const { id, name, user_type } = user;
+
+            // Cria o token
+            const token = jwt.sign({id, name, user_type}, process.env.TOKEN_SECRET, {
+                expiresIn: process.env.TOKEN_EXPIRATION
+            });
+
+            res.json({success: true, token });
         }catch (e) {
-            res.status(httpStatusCode.SERVER_ERROR).json({success: false, message: "Erro ao carregar tipos de usuários. Tente mais tarde!"})
+            res.status(httpStatusCode.SERVER_ERROR).json({success: false, message: "Erro ao logar usuário. Tente mais tarde!"})
         }
     }
 }
