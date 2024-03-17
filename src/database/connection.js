82c0161ba83477f37be89f889805429522e3bf15
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 21:10:01 +0100

    Método de verificação e associação de models atualizado

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,18 +8,6 @@ import User from "../models/User";
 const connection = new Sequelize(dbConfig); // Instância a conn. com a db
 
 // Guarda todos os models da bd
-const models = {
-    UserType, User
-};
-
-// Insere a conexão nos models
-Object.keys(models).forEach(model => {
-    models[model].init(connection)
-})
-
-// Verifica se um model possui associação e cria, caso possua
-Object.keys(models).forEach(model => {
-    if (models[model].associate) {
-        models[model].associate(models)
-    }
-})
+const models = [UserType, User];
+models.forEach(model => model.init(connection)); // Inicia os models com a conexão com o banco
+models.forEach(model => model.associate && model.associate(connection.models)) // Verifica os models que possuem o método associate e executa passando o array de models
