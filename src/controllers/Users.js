339eba25b5bb21e2e261b44df338292ff0414e19
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 06:06:08 +0100

    Adicionado retorno dos user com os joins

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,10 +1,22 @@
 import httpStatusCode from "../utils/HttpStatusCode";
 import User from "../models/User";
+import UserType from "../models/UserType";
 class Users {
     async get(req, res){
         try {
-            const users = await User.findAll();
-            return res.json(users);
+            const users = await User.findAll({
+                    attributes: ['users_id', 'name', 'email'], // Define os campos da tabela da table main
+                    // Define as associações (joins)
+                    include: [
+                        // Associação com model user_types
+                        {
+                            model: UserType,
+                            as: "type", // Referência o alias definido na assoc.
+                            attributes: ['user_types_id', 'name'] // Define os campos a serem recuperados de user_types
+                        }
+                    ],
+            });
+            res.json(users);
         }catch (e) {
             res.status(httpStatusCode.SERVER_ERROR).json({ success: false, message: e.message })
         }
