9fcb64e28f57c4ea33c193db64e8a44a6133365c
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 12:38:31 +0100

    middleware admin adicionada as rotas users e userTypes

--- src/routes/userTypes.js
+++ src/routes/userTypes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import userTypesController from '../controllers/UserTypes';
-import auth from "../middlewares/auth"; // Importa o objecto controller de UserTypes
+import auth from "../middlewares/auth";
+import admin from "../middlewares/admin"; // Importa o objecto controller de UserTypes
 const router = new Router; // Cria o router
 
 // Rotas
 // UserTypes
-router.get('/', auth , userTypesController.index); // getAll | index
+router.get('/', auth, admin , userTypesController.index); // getAll | index
 
 export default router; // Exporta o router
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import userController from '../controllers/Users';
-import auth from "../middlewares/auth"; // Importa o objecto controller de User
+import auth from "../middlewares/auth";
+import admin from "../middlewares/admin"; // Importa o objecto controller de User
 const router = new Router;
 
-router.get('/', auth, userController.index);
-router.get('/:id', auth, userController.show);
-router.post('/create', auth, userController.create);
-router.put('/:id', auth, userController.put);
-router.delete('/:id', auth, userController.delete);
+router.get('/', auth, admin, userController.index);
+router.get('/:id', auth, admin, userController.show);
+router.post('/create', auth, admin, userController.create);
+router.put('/:id', auth, admin, userController.put);
+router.delete('/:id', auth, admin, userController.delete);
 
 
 export default router; // Exporta o router
