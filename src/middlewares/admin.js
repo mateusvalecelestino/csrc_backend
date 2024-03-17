f1a7c4aa0e71d592b562b26df5bcbf540f56335e
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-17 12:37:15 +0100

    middleware admin criado

---
+++ src/middlewares/admin.js
@@ -0,0 +1,7 @@
+import httpStatusCode from "../utils/HttpStatusCode";
+// Middleware para verificar se o user é admin
+export default (req, res, next) => {
+   const ADMIN_USER_TYPE = 1;
+   if(req.userId !== 1) return res.status(httpStatusCode.UNAUTHORIZED).json({ success: false, message: "Acesso negado!"} );
+   return next();
+}
