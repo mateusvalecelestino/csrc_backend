cf0f0b2caf850690e3a1a12440f814c97b416da1
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-18 16:26:28 +0100

    Adicionada a verificação de estado da conta após a verificação do token no middleware auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,10 @@
 import httpStatusCode from "../utils/HttpStatusCode";
 import jwt from "jsonwebtoken";
-import 'dotenv/config' // Importa o dotenv
+import 'dotenv/config'
+import User from "../models/User"; // Importa o dotenv
 
 // Middleware para verificar se o token do usuário é válido
-export default (req, res, next) => {
+export default async (req, res, next) => {
     const {authorization} = req.headers; // Guarda o token de headers
 
     // Verifica se foi mandado um token
@@ -13,11 +14,17 @@ export default (req, res, next) => {
     try {
         const userData = jwt.verify(token, process.env.TOKEN_SECRET); // Verifica o token
         const {id, user_type} = userData;
+
+        const user = await User.findByPk(id); // Busca os dados do usuário no banco
+
+        // Verifica se o usuário ainda é activo no banco
+        if(!user || !user.active) return res.status(httpStatusCode.UNAUTHORIZED).json({ message: "Acesso negado sua conta foi desactivada." })
+
         // Adiciona as info do usuário na requisição
         req.userId = id;
         req.userType = user_type;
         return next();
     } catch (e) {
-        return res.status(httpStatusCode.BAD_REQUEST).json({ message: "token expirado ou inválido." });
+        return res.status(httpStatusCode.BAD_REQUEST).json({ message: "Token expirado ou inválido." });
     }
 }
