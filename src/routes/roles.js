7fb9f3aa78f396a55c3ea49cb256e713e600636c
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 09:06:43 +0100

    getHandler adicionado as rotas get de users e roles

--- src/routes/roles.js
+++ src/routes/roles.js
@@ -2,9 +2,10 @@ import {Router} from "express";
 import roleController from '../controllers/Roles';
 import auth from "../middlewares/auth";
 import admin from "../middlewares/admin";
+import getHandler from "../middlewares/getHandler";
 const router = new Router;
 
-router.get('/', auth, admin, roleController.index);
+router.get('/', auth, admin, getHandler, roleController.index);
 // router.get('/:id', auth, admin, roleController.show);
 // router.post('/create', auth, admin, roleController.create);
 // router.put('/:id', auth, admin, roleController.put);
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,11 @@ import {Router} from "express";
 import userController from '../controllers/Users';
 import auth from "../middlewares/auth";
 import admin from "../middlewares/admin";
-import updateUser from "../middlewares/updateUser"; // Importa o objecto controller de User
+import updateUser from "../middlewares/updateUser";
+import getHandler from "../middlewares/getHandler"; // Importa o objecto controller de User
 const router = new Router;
 
-router.get('/', auth, admin, userController.index);
+router.get('/', auth, admin, getHandler, userController.index);
 router.get('/:id', auth, admin, userController.show);
 router.post('/create', auth, admin, userController.create);
 router.put('/:id', auth, updateUser, userController.put);
