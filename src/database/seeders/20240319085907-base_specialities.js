6a0444e67f80569a623555ba14960f5951df551f
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 10:02:06 +0100

    seed de especialidades base criada

---
+++ src/database/seeders/20240319085907-base_specialities.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('specialities', [
+      {
+        name: 'Geral',
+        created_by: 1,
+        updated_by: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        name: 'Pediatria',
+        created_by: 1,
+        updated_by: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('specialities', null, {});
+  }
+};
