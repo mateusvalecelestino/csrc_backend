3488634edc120ce67a0039b86ead902158269c89
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 08:31:36 +0100

    Seed para criar cargos bases adicionado

---
+++ src/database/seeders/20240319072614-base_roles.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('roles', [
+      {
+        name: 'Enfermeiro',
+        desc: "Realiza coisas de enfermeiros.",
+        created_by: 1,
+        updated_by: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },      {
+        name: 'Médico',
+        desc: "Realiza coisas de médico.",
+        created_by: 1,
+        updated_by: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('roles', null, {});
+  }
+};
