f56b55af3623fbfd607c4fbad5b899ebc989e479
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 21:43:38 +0100

    seed de base users alterada para corresponder a alteração dos campos

--- src/database/seeders/20240316193441-generate_base_users.js
+++ src/database/seeders/20240316193441-generate_base_users.js
@@ -5,8 +5,8 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.bulkInsert('users', [
             {
-                name: 'Admin',
-                email: 'admin@admin.com',
+                username: 'Admin',
+                user_email: 'admin@admin.com',
                 password_hash: '$2a$08$CTr1hm80.Dn9a9tAjWFrtOZZmbYa/uSQE01IY.9nLuYWyShgFyzYi', // Csrc-1234
                 user_type: 1,
                 created_by: 1,
@@ -15,8 +15,8 @@ module.exports = {
                 updated_at: new Date()
             },
             {
-                name: 'Medico',
-                email: 'medico@medico.com',
+                username: 'Medico',
+                user_email: 'medico@medico.com',
                 password_hash: '$2a$08$CTr1hm80.Dn9a9tAjWFrtOZZmbYa/uSQE01IY.9nLuYWyShgFyzYi', // Csrc-1234
                 user_type: 2,
                 created_by: 1,
@@ -25,8 +25,8 @@ module.exports = {
                 updated_at: new Date()
             },
             {
-                name: 'Anal. Lab',
-                email: 'anal@anal.com',
+                username: 'Anal. Lab',
+                user_email: 'anal@anal.com',
                 password_hash: '$2a$08$CTr1hm80.Dn9a9tAjWFrtOZZmbYa/uSQE01IY.9nLuYWyShgFyzYi', // Csrc-1234
                 user_type: 3,
                 created_by: 1,
@@ -35,8 +35,8 @@ module.exports = {
                 updated_at: new Date()
             },
             {
-                name: 'Triagista',
-                email: 'triagista@triagista.com',
+                username: 'Triagista',
+                user_email: 'triagista@triagista.com',
                 password_hash: '$2a$08$CTr1hm80.Dn9a9tAjWFrtOZZmbYa/uSQE01IY.9nLuYWyShgFyzYi', // Csrc-1234
                 user_type: 4,
                 created_by: 1,
@@ -45,8 +45,8 @@ module.exports = {
                 updated_at: new Date()
             },
             {
-                name: 'Recepcionista',
-                email: 'recepcionista@recepcionista.com',
+                username: 'Recepcionista',
+                user_email: 'recepcionista@recepcionista.com',
                 password_hash: '$2a$08$CTr1hm80.Dn9a9tAjWFrtOZZmbYa/uSQE01IY.9nLuYWyShgFyzYi', // Csrc-1234
                 user_type: 5,
                 created_by: 1,
