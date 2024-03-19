203b54f697b113a3eeb3da1a3f252499b3cdd6cd
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 18:54:49 +0100

    Mudança na nomenclatura dos controllers, adopção do singular

--- src/routes/tokens.js
+++ src/routes/tokens.js
@@ -1,8 +1,8 @@
 import {Router} from "express";
-import tokensController from '../controllers/Tokens'; // Importa o objecto controller de UserTypes
+import tokenController from '../controllers/Tokens'; // Importa o objecto controller de UserTypes
 const router = new Router; // Cria o router
 
 // Rotas
-router.post('/', tokensController.create); // generate
+router.post('/', tokenController.create); // generate
 
 export default router; // Exporta o router
--- src/routes/userTypes.js
+++ src/routes/userTypes.js
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import userTypesController from '../controllers/UserTypes';
+import userTypeController from '../controllers/UserTypes';
 import auth from "../middlewares/auth";
 import admin from "../middlewares/admin"; // Importa o objecto controller de UserTypes
 const router = new Router; // Cria o router
 
 // Rotas
 // UserTypes
-router.get('/', auth, admin, userTypesController.index); // getAll | index
+router.get('/', auth, admin, userTypeController.index); // getAll | index
 
 export default router; // Exporta o router
