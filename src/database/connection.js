06c89958df209484488f5d58f54839ee9e219b74
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-19 08:02:33 +0100

    Adicionado hookglobal em todos os models para realizar um trim nos dados antes da inserção no banco

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,34 @@
 import Sequelize from "sequelize";
 import dbConfig from "../config/database";
-
-// Importa todos os models da bd
 import UserType from "../models/UserType";
 import User from "../models/User";
+import Role from "../models/Role";
 
 const connection = new Sequelize(dbConfig); // Instância a conn. com a db
 
 // Guarda todos os models da bd
-const models = [UserType, User];
-// Inicia os models com a conexão com o banco
-models.forEach(model => model.init(connection));
+const models = [UserType, User, Role];
+
+// Inicia os models com a conexão com o banco e adiciona o hook global
+models.forEach(model => {
+    // Adiciona a conexão a todos os models
+    model.init(connection);
+
+    // Adicione um hook global 'beforeSave' para todos os modelos
+    connection.addHook('beforeSave', "trimAllValues" , (instance) => {
+        // Verifica se a instância é do model actual
+        if (instance instanceof model) {
+            // Realiza o trim dos dados de string antes de salvar
+            Object.keys(instance.dataValues).forEach(key => {
+                // Verifica se o valor do atributo é uma string
+                if (typeof instance[key] === 'string') {
+                    // Remove espaços extras entre as palavras
+                    instance[key] = instance[key].replace(/\s+/g, ' ').trim();
+                }
+            });
+        }
+    });
+});
+
 // Verifica os models que possuem o método associate e executa passando o array de models
 models.forEach(model => model.associate && model.associate(connection.models))
