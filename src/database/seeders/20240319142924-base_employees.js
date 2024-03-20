0f5e464238252b703925ee8e8a425c0ce5580174
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-20 21:47:28 +0100

    Inserção de funcionário base recepcionista

--- src/database/seeders/20240319142924-base_employees.js
+++ src/database/seeders/20240319142924-base_employees.js
@@ -5,7 +5,7 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.bulkInsert('employees', [
             {
-                full_name: 'employee um',
+                full_name: 'Recepcionista',
                 birth_date: "2024-03-19",
                 gender: "M",
                 order_number: "1234567890",
@@ -13,7 +13,7 @@ module.exports = {
                 tel: "123-123-123",
                 role_id: "1",
                 specialty_id: "1",
-                user_id: "1",
+                user_id: "5",
                 created_by: "1",
                 updated_by: "1",
                 created_at: new Date(),
