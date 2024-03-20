f01acddcfa249407093941d66164eaeb564c45ee
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-20 11:39:50 +0100

    Remoção da rota create de users

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -66,27 +66,6 @@ class Users {
         }
     }
 
-    async create(req, res) {
-        try {
-
-            // Remoção dos campos não criáveis
-            delete req.body.id;
-            delete req.body.active;
-            delete req.body.created_at;
-            delete req.body.updated_at;
-
-            // Adiciona o usuário que fez a request como criador e actualizador
-            req.body.created_by = req.userId;
-            req.body.updated_by = req.userId;
-
-            const user = await User.create(req.body);
-            const {id, username, user_email, user_type, created_by} = user;
-            return res.status(httpStatusCode.CREATED).json({user: {id, username, user_email, user_type, created_by}});
-        } catch (error) {
-            errorHandler(error, req, res);
-        }
-    }
-
     async put(req, res) {
         try {
             if (!isInt(req.params.id)) return res.status(httpStatusCode.BAD_REQUEST).json({message: "id inválido."});
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,7 +8,6 @@ const router = new Router;
 
 router.get('/', auth, admin, getHandler, userController.index);
 router.get('/:id', auth, admin, userController.show);
-router.post('/create', auth, admin, userController.create);
 router.put('/:id', auth, updateUser, userController.put);
 router.patch('/:id/active', auth, admin, userController.patch);
 export default router; // Exporta o router
