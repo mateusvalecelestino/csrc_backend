c535999c3c79123d1085b8733e34c51a9ca2916e
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-20 21:01:24 +0100

    migration para criar a table patients_address criada

---
+++ src/database/migrations/20240320194919-create_table_patients_addresses.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('patients_addresses', {
+      id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      street: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      patient_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+          model: 'patients',
+          key: 'id',
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        }
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('patients_addresses');
+  }
+};
