15798fbc9f566f397913bb39a56bc559245be251
mateusvalecelestino <127565212+mateusvalecelestino@users.noreply.github.com>
2024-03-21 01:08:08 +0100

    migration para alterar a restrição de fk na table employees_contacts criada

---
+++ src/database/migrations/20240321000047-add_constraint_on_delete_cascade_na_table_employees_contacts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    // Altera a restrição de FK
+    await queryInterface.sequelize.query('ALTER TABLE employees_contacts DROP FOREIGN KEY employees_contacts_ibfk_1;');
+    await queryInterface.sequelize.query('ALTER TABLE employees_contacts ADD CONSTRAINT employees_contacts_ibfk_1 FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE ON UPDATE CASCADE;');
+  },
+
+  async down (queryInterface) {
+    // Devolve a restrição de FK
+    await queryInterface.sequelize.query('ALTER TABLE employees_contacts DROP FOREIGN KEY employees_contacts_ibfk_1;');
+    await queryInterface.sequelize.query('ALTER TABLE employees_contacts ADD CONSTRAINT employees_contacts_ibfk_1 FOREIGN KEY (employee_id) REFERENCES employees(id);');
+  }
+};
